4d9e2f587cb461681bbf34d4a025c576ee2bd04e
AngeviLee <568178364@qq.com>
2020-04-26 19:02:57 +0800

    微信公众号-通过jssdk辅助工具验证权限

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express')
 // 引入路径-返回path的目录名字
 const path = require('path')
 
+// 引入 jssdk 辅助工具
+const { Wechat } = require('wechat-jssdk')
+const wx = new Wechat({
+    appId: "wx2938c8d29780dc1d",  // 开发阶段我们使用的是测试账户下的appId
+    appSecret: "317c95e2233628579b1b71d9d1444fb7"  // 开发阶段我们使用的是测试账户下的appsecret
+})
+
 // 调用 express 框架
 const app = express()
 
@@ -22,5 +29,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 // 服务器定义相关路由来等待浏览器的请求
 app.get('/', (req, res) => {
     // res.send('Hello Word!')
-    res.render('index')
+
+    // 通过此方法可以获取 config 接口注入权限验证配置的参数
+    // 此处的 Your url 指的是使用 JS-SDK 那个页面的地址
+    wx.jssdk.getSignature('http://localhost:3000/').then(signatureData => {
+        console.log(signatureData)
+        // 将生成的参数传入模板
+        res.render('index', signatureData)
+    })
+
 })
