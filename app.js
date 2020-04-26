cd39a355642ca5df88ed764009e05b6cd28d67ee
AngeviLee <568178364@qq.com>
2020-04-26 17:06:55 +0800

    微信公众号-使用xtpl模板引擎渲染模板网页|相当于准备微信网页

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 // 引入express
 const express = require('express')
 
+// 引入路径-返回path的目录名字
+const path = require('path')
+
 // 调用 express 框架
 const app = express()
 
 // 监听3000端口，相当于拥有服务器
 app.listen(3000)
 
+// 配置xtpl模板引擎-要求模板是以 xtpl 后缀名的
+app.set('view engine', 'xtpl')
+
+// 配置模板目录
+app.set('views', path.join(__dirname, 'views'))
+
 // 服务器定义相关路由来等待浏览器的请求
 app.get('/', (req, res) => {
-    res.send('Hello Word!')
+    // res.send('Hello Word!')
+    res.render('index')
 })
